44cdf421012ffa160752f813441dbea7a1f27964
vetal7777 <jobschildren@gmail.com>
2023-12-30 22:40:29 +0200

    refactor: code style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,29 @@
 /** @type {import('tailwindcss').Config} */
-const colors = require("tailwindcss/colors");
+const colors = require('tailwindcss/colors')
 
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     colors: {
       ...colors,
-      "chad-blue-0": "#32ABF2",
-      "chad-blue-30": "#96CAF7",
-      "md-dark-blue-20": "#134267",
-      "md-dark-blue-60": "#5D7FA3",
+      'chad-blue-0': '#32ABF2',
+      'chad-blue-30': '#96CAF7',
+      'md-dark-blue-20': '#134267',
+      'md-dark-blue-60': '#5D7FA3'
     },
     extend: {
       backgroundImage: {
-        "linear-gradient":
-          "linear-gradient(339deg, #0D3251 0%, #19476C 103.05%)",
+        'linear-gradient':
+          'linear-gradient(339deg, #0D3251 0%, #19476C 103.05%)'
       },
       spacing: {
-        "stat-slide": "364px",
-        "steps-gap": "48px",
-        "2px": "2px",
-        "step-line-50%": "calc(50% - 1px)",
-        "step-line-100%": "calc(100% + 2px)",
-      },
-    },
+        'stat-slide': '364px',
+        'steps-gap': '48px',
+        '2px': '2px',
+        'step-line-50%': 'calc(50% - 1px)',
+        'step-line-100%': 'calc(100% + 2px)'
+      }
+    }
   },
-  plugins: [],
-};
+  plugins: []
+}
