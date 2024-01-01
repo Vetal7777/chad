8ae780acca03e0bdb484b577f18701a7f1dd97ec
vetal7777 <jobschildren@gmail.com>
2024-01-01 04:13:12 +0200

    refactor: code style

--- src/components/steps-item/index.tsx
+++ src/components/steps-item/index.tsx
@@ -4,13 +4,13 @@ import './steps-item.scss'
 export function StepsItem({ item, renderLine = false }: StepsItemProps) {
   const { title } = item
 
-  const notRenderLineClass = renderLine ? '' : 'line-hidden'
+  const withoutLineClass = renderLine ? '' : 'line-hidden'
 
   return (
     <>
       <div className="flex items-center gap-4 text-base font-medium">
         <div
-          className={`after:h-100% relative h-8 w-8 rounded-full border-2 border-md-dark-blue-60 after:absolute after:left-step-line-50% after:right-step-line-50% after:top-step-line-100% after:h-steps-gap after:w-2px after:bg-md-dark-blue-60  after:content-[''] ${notRenderLineClass}`}
+          className={`after:h-100% relative h-8 w-8 rounded-full border-2 border-md-dark-blue-60 after:absolute after:left-step-line-50% after:right-step-line-50% after:top-step-line-100% after:h-steps-gap after:w-2px after:bg-md-dark-blue-60  after:content-[''] ${withoutLineClass}`}
         />
         <p className={`text-md-dark-blue-60`} children={title} />
       </div>
