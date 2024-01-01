dd507fbab313ad4cf09b0e19bad593ad8c169ab1
vetal7777 <jobschildren@gmail.com>
2024-01-01 04:07:05 +0200

    refactor: code style

--- src/components/steps-item/index.tsx
+++ src/components/steps-item/index.tsx
@@ -1,35 +1,18 @@
-import { StepStatus, type StepsItemProps } from '@/@types'
-import { useMemo } from 'react'
+import type { StepsItemProps } from '@/@types'
+import './steps-item.scss'
 
 export function StepsItem({ item, renderLine = false }: StepsItemProps) {
   const { title } = item
 
-  const lineClass = useMemo(() => {
-    return renderLine
-      ? "relative after:w-2px after:left-step-line-50% after:right-step-line-50% after:top-step-line-100% after:h-100% after:absolute after:h-steps-gap after:bg-md-dark-blue-60 after:content-['']"
-      : ''
-  }, [renderLine])
-
-  const indicatorClass = useMemo(() => {
-    switch (item.status) {
-      default:
-        return 'border-md-dark-blue-60'
-    }
-  }, [item.status])
-
-  const titleClass = useMemo(
-    () =>
-      item.status === StepStatus.none ? 'text-md-dark-blue-60' : 'text-white',
-    [item.status]
-  )
+  const notRenderLineClass = renderLine ? '' : 'line-hidden'
 
   return (
     <>
       <div className="flex items-center gap-4 text-base font-medium">
         <div
-          className={`h-8 w-8 rounded-full border-2 ${indicatorClass} ${lineClass}`}
+          className={`after:h-100% relative h-8 w-8 rounded-full border-2 border-md-dark-blue-60 after:absolute after:left-step-line-50% after:right-step-line-50% after:top-step-line-100% after:h-steps-gap after:w-2px after:bg-md-dark-blue-60  after:content-[''] ${notRenderLineClass}`}
         />
-        <p className={titleClass} children={title} />
+        <p className={`text-md-dark-blue-60`} children={title} />
       </div>
     </>
   )
