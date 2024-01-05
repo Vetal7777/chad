79b100f54b083352346a0ac438cec55ae13c35d0
vetal7777 <jobschildren@gmail.com>
2024-01-05 02:46:49 +0200

    refactor: route config

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { AuthProvider } from '@/context/auth'
 import '@/i18n/config'
 import { MainLayout } from '@/layouts/main'
 import { Auth } from '@/pages/auth'
-import { Home } from '@/pages/home'
 import { Route, Routes } from 'react-router-dom'
 import 'swiper/css'
 import 'swiper/css/pagination'
@@ -11,8 +10,8 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
-        <Route index element={<Home />} />
-        <Route path="auth" element={<AuthProvider children={<Auth />} />} />
+        {/* <Route index element={<Home />} /> */}
+        <Route index element={<AuthProvider children={<Auth />} />} />
       </Route>
     </Routes>
   )
