6fffca97b6122644d2d62229c57da1c462c01a34
vetal7777 <jobschildren@gmail.com>
2024-01-05 02:33:42 +0200

    refactor: code style

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { AuthProvider } from '@/context/auth'
 import '@/i18n/config'
+import { MainLayout } from '@/layouts/main'
 import { Auth } from '@/pages/auth'
 import { Home } from '@/pages/home'
 import { Route, Routes } from 'react-router-dom'
 import 'swiper/css'
 import 'swiper/css/pagination'
-import { MainLayout } from './layouts/main'
 
 function App() {
   return (
